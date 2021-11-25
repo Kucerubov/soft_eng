3718e54fecb0771737f10a8c7f2dd9971d95a2cb
MrNeahka <kucerubov365@gmail.com>
2021-11-26 00:25:16 +0200

    Beta

--- src/app/src/main/java/com/example/genlib/LoginActivity.java
+++ src/app/src/main/java/com/example/genlib/LoginActivity.java
@@ -1,37 +1,56 @@
 package com.example.genlib;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.net.PasswordAuthentication;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private EditText login_user;
-    private EditText password_user;
-    private Button login_button;
-    private Button register_button;
+    Button btnSignIn, btnRegister;
+    FirebaseAuth auth;
+    FirebaseDatabase db;
+    DatabaseReference users;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
 
-        login_user = findViewById(R.id.login_app);
-        password_user = findViewById(R.id.password_app);
-        login_button = findViewById(R.id.login);
-        register_button = findViewById(R.id.register);
+        btnSignIn = findViewById(R.id.login);
+        btnRegister = findViewById(R.id.register);
 
-        login_button.setOnClickListener(new View.OnClickListener() {
+        auth = FirebaseAuth.getInstance();
+        db = FirebaseDatabase.getInstance();
+        users = db.getReference("Users");
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-
+            public void onClick(View v) {
+                showRegisterWindow();
             }
         });
+
+    }
+
+    private void showRegisterWindow() {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Зарегистрироваться");
+        dialog.setMessage("Введите все данные для регистрации");
+
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View register_window = inflater.inflate(R.layout.login_window);
+
     }
 
 }
